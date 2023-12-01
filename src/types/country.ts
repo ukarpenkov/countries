896dac166cb423b8e82a8e2284b4bf1ebc0d4537
ukarpenkov <truloveu@gmail.com>
2023-12-01 14:50:57 +0300

    base types

---
+++ src/types/country.ts
@@ -0,0 +1,28 @@
+import { Region } from './regions'
+
+type Currency = {
+  code: string
+  name: string
+  symbol: string
+}
+type Languages = {
+  iso639_1: string
+  iso639_2: string
+  name: string
+  nativeName: string
+}
+
+export type Country = {
+  name: string
+  nativename: string
+  flag: string
+  flags: [{ png: string }, { svg: string }]
+  region: Region
+  subregion: string
+  population: number
+  capital: string
+  topLevelDomain: string[]
+  borders: string[]
+  currencies: Currency[]
+  languages: Languages[]
+}
