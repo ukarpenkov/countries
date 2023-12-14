14679309e1f5c1d15c2e48efab5af72d39ba4601
ukarpenkov <truloveu@gmail.com>
2023-12-14 15:03:22 +0300

    controls types

--- src/features/controls/CustomSelect.tsx
+++ src/features/controls/CustomSelect.tsx
@@ -1,12 +1,20 @@
 import styled from 'styled-components'
 
 import Select, { Props } from 'react-select'
+import { Region } from 'types'
 
-function MySelect(props: Props) {
-  return <Select />
+export type CountryOption =
+  | {
+      label: Region
+      value: Region
+    }
+  | ''
+
+function MySelect(props: Props<CountryOption, false>) {
+  return <Select {...props} />
 }
 
-export const CustomSelect = styled(Select).attrs({
+export const CustomSelect = styled(MySelect).attrs({
   styles: {
     control: (provided) => ({
       ...provided,
--- src/features/controls/use-region.ts
+++ src/features/controls/use-region.ts
@@ -3,13 +3,22 @@ import { useSelector, useDispatch } from 'react-redux'
 import { setRegion } from './controls-slice'
 import { selectRegion } from './controls-selectors'
 import { useAppDispatch } from 'store'
+import { CountryOption } from './CustomSelect'
+import { Region } from 'types'
+import { SingleValue } from 'react-select'
 
-export const useRegion = () => {
+type onSelect = (reg: SingleValue<CountryOption>) => void
+
+export const useRegion = (): [Region | '', onSelect] => {
   const dispatch = useAppDispatch()
   const region = useSelector(selectRegion)
 
-  const handleSelect = (reg) => {
-    dispatch(setRegion(reg?.value || ''))
+  const handleSelect: onSelect = (reg) => {
+    if (reg) {
+      dispatch(setRegion(reg.value))
+    } else {
+      dispatch(setRegion(''))
+    }
   }
 
   return [region, handleSelect]
