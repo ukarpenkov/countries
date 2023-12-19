df3addf1a2dd03e2dfd90c654b8440ff8a5f3181
ukarpenkov <truloveu@gmail.com>
2023-12-19 13:27:12 +0300

    run project

--- src/features/countries/use-countries.ts
+++ src/features/countries/use-countries.ts
@@ -1,7 +1,6 @@
 import { useSelector } from 'react-redux'
 import { useEffect } from 'react'
 
-import { selectControls } from '../controls/controls-slice'
 import { loadCountries } from './countries-slice'
 import {
   selectCountriesInfo,
@@ -9,6 +8,7 @@ import {
 } from './countries-selectors'
 import { RootState, useAppDispatch } from 'store'
 import { Country } from 'types'
+import { selectControls } from 'features/controls/controls-selectors'
 
 export const useCountries = (): [
   Country[],
